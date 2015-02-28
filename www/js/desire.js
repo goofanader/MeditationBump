0a354920f1b2d268cdc8dd3e213b5fde9a8a44e3
Andrew Elliott <adeedadude@gmail.com>
2015-02-28 09:55:02 -0800

    Whoops added var

--- www/js/desire.js
+++ www/js/desire.js
@@ -8,8 +8,8 @@ Desire.prototype.constructor = Actor
 
 Desire.prototype.update = function() {
 	// Updating acceleration every tick based on gravity
-	toCenter = monk.position.nonImmuteSub(position);
-	a_magnitude = GRAVITY * monk.mass / (Math.pow(toCenter.magnitude,2));
-	acceleration = toCenter.norm.scale(a_magnitude);
+	var toCenter = monk.position.nonImmuteSub(position);
+	var a_magnitude = GRAVITY * monk.mass / (Math.pow(toCenter.magnitude,2));
+	var acceleration = toCenter.norm.scale(a_magnitude);
 	super.update();
 }
--- www/js/projection.js
+++ www/js/projection.js
@@ -8,7 +8,7 @@ Projection.prototype.constructor = Actor
 
 Projection.prototype.update = function() {
 	// Updating acceleration every tick based on spring & damper physics
-	positionFromCenter = position.nonImmuteSub(monk.position);
-	acceleration = positionFromCenter.scale(-SPRING_CONSTANT).nonImmuteSub(velocity.scale(DAMPING)).scale(1/mass);
+	var positionFromCenter = position.nonImmuteSub(monk.position);
+	var acceleration = positionFromCenter.scale(-SPRING_CONSTANT).nonImmuteSub(velocity.scale(DAMPING)).scale(1/mass);
 	super.update();
 }
