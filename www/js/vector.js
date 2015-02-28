354dd78deb5fb638faafabe2eda66d5cba0f1130
Paul Fallon <pfallon@calpoly.edu>
2015-02-27 20:22:40 -0800

    Vector class update

--- www/js/vector.js
+++ www/js/vector.js
@@ -4,15 +4,18 @@ function Vector(x, y) {
 }
 
 Vector.prototype.add(other) {
-    return new Vector(this.x + other.x, this.y + other.y);
+    this.x = this.x + other.x;
+    this.y = this.y + other.y;
 }
 
 Vector.prototype.sub(other) {
-    return new Vector(this.x + other.x, this.y + other.y);   
+    this.x = this.x + other.x;
+    this.y = this.y + other.y;   
 }
 
 Vector.prototype.scale(scale) {
-    return new Vector(scale * this.x, scale * this.y);
+    this.x = scale * this.x;
+    this.y = scale * this.y;
 }
 
 Vector.prototype.dot(other) {
@@ -20,7 +23,9 @@ Vector.prototype.dot(other) {
 }
 
 Vector.prototype.cross(other) {
-
+    size_this = Math.sqrt(Math.pow(this.x,2), Math.pow(this.y,2))
+    size_other = Math.sqrt(Math.pow(other.x,2), Math.pow(other.y,2))
+    return Math.pow(size_this, 2) * Math.pow(size_other, 2) - Math.pow(this.dot(other),2)
 }
 
 Vector.prototype.equals(other) {
