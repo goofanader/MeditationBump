de6a62344ec1fb39a66960d9c02309cefe92c8b6
Paul Fallon <pfallon@calpoly.edu>
2015-02-27 20:30:57 -0800

    Vector class update

--- www/js/vector.js
+++ www/js/vector.js
@@ -1,33 +1,33 @@
-function Vector(x, y) {
+function Vector = function(x, y) {
     this.x = x;
     this.y = y;
 }
 
-Vector.prototype.add(other) {
+Vector.prototype.add = function(other) {
     this.x = this.x + other.x;
     this.y = this.y + other.y;
 }
 
-Vector.prototype.sub(other) {
+Vector.prototype.sub = function(other) {
     this.x = this.x + other.x;
     this.y = this.y + other.y;   
 }
 
-Vector.prototype.scale(scale) {
+Vector.prototype.scale = function(scale) {
     this.x = scale * this.x;
     this.y = scale * this.y;
 }
 
-Vector.prototype.dot(other) {
+Vector.prototype.dot = function(other) {
     return this.x * other.x + this.y * other.y;
 }
 
-Vector.prototype.cross(other) {
-    size_this = Math.sqrt(Math.pow(this.x,2), Math.pow(this.y,2))
-    size_other = Math.sqrt(Math.pow(other.x,2), Math.pow(other.y,2))
-    return Math.pow(size_this, 2) * Math.pow(size_other, 2) - Math.pow(this.dot(other),2)
+Vector.prototype.cross = function(other) {
+    size_this = Math.sqrt(Math.pow(this.x, 2), Math.pow(this.y, 2));
+    size_other = Math.sqrt(Math.pow(other.x, 2), Math.pow(other.y, 2));
+    return Math.pow(size_this, 2) * Math.pow(size_other, 2) - Math.pow(this.dot(other), 2);
 }
 
-Vector.prototype.equals(other) {
+Vector.prototype.equals = function(other) {
     return (this.x == other.x && this.y == other.y);
 }
